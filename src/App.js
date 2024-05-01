1fcd2513a9b8852404853c94cffce7432d4c7adf
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-05-01 20:50:34 +0530

    basic  js - 2

--- src/App.js
+++ src/App.js
@@ -3,14 +3,25 @@ import './App.css';
 function App() {
   let name = "Het";
   let l = [10, 20, 30];
+  let obj = {
+    'name': 'Het',
+    'learning': 'React',
+    'desc': 'I am learning React.'
+  }
+  let status = true;
   return (
     <div className="App">
       <h1>Welcome, {name} to the Website</h1>
-      {l.map((v) => {
-        return (
-          <div>{v}</div>
-        )
-      })}
+      {
+        l.map((v) => {
+          return (
+            <div>{v}</div>
+          );
+        })
+      }
+      <div>{obj.name}</div>
+      <div>{10 + 20}</div>
+      {(status) ? <h1 style={{ color: "red" }}>Welcome</h1> : <h1>Not Welcome</h1>}
     </div>
   );
 }
