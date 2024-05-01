bf4f464c010440feefc1abdadd719ca4759ffdb6
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-05-01 20:32:17 +0530

    basic js - 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let name = "Het";
+  let l = [10, 20, 30];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome, {name} to the Website</h1>
+      {l.map((v) => {
+        return (
+          <div>{v}</div>
+        )
+      })}
     </div>
   );
 }
