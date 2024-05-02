7c356998c086d7f426450a110976f653e2d6d2c1
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-05-02 12:14:07 +0530

    react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Col, Container, Row, Card, Button } from 'react-bootstrap';
 import './App.css';
 import Footer from './Footer';
 import Header from './Header';
@@ -6,9 +7,9 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <div className='container'>
+      {/* <div className='container'>
         <h1 className='text-danger'>Welcome to Page</h1>
-      </div>
+      </div> */}
       {/* <div className='row'>
         <Card />
         <Card />
@@ -18,6 +19,66 @@ function App() {
         <Card />
         <Card />
       </div> */}
+      <Container fluid>
+        <Container>
+          <Row>
+            <Col className='col-12 text-center py-4'>
+              <h1>Our Courses</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg='3' md='6'>
+              <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                  <Card.Title>MEARN Stack Developer</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col lg='3' md='6'>
+              <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                  <Card.Title>MEAN stack Developer</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col lg='3' md='6'>
+              <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                  <Card.Title>Flutter Developer</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col lg='3' md='6'>
+              <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                  <Card.Title>Python Developer</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+
       <Footer />
     </div>
   );
@@ -25,8 +86,8 @@ function App() {
 
 export default App;
 
-function Card() {
-  return (
-    <div className='cardItems'>Card div</div>
-  )
-}
+// function Card() {
+//   return (
+//     <div className='cardItems'>Card div</div>
+//   )
+// }
